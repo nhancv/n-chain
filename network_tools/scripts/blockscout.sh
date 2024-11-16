d5cef9481793773ac7b895ce15c548a590e46bde
Nhan Cao <nhancv92@gmail.com>
2024-11-16 11:14:55 +0700

    feat: update blockscout

--- network_tools/scripts/blockscout.sh
+++ network_tools/scripts/blockscout.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-# Install aws cli
-apt update -y
-apt install unzip -y
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-./aws/install
+
 export EC2_USER=ubuntu
 
 # Install docker
